3b0888a11d21f3f52017e04329b1f1eb166bf566
Paulo Lopes <paulohdas.lopes@gmail.com>
2021-11-06 18:10:12 -0300

    :construction: refact question

--- lib/main.dart
+++ lib/main.dart
@@ -9,46 +9,61 @@ void main() {
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
 
+  final _perguntas = const [
+    {
+      'texto': 'Qual a sua idade?',
+      'respostas': ['15', '20', '25', '27']
+    },
+    {
+      'texto': 'Qual o seu signo?',
+      'respostas': ['Peixes', 'Capricornio', 'Sagitario', 'Touro']
+    },
+    {
+      'texto': 'Qual a sua cor favorita?',
+      'respostas': ['Azul', 'Vermelho', 'Verde', 'Amarelo']
+    },
+  ];
+
+
   void _responder() {
-    setState(() {
-      _perguntaSelecionada++;
-    });
+    if(temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+      });
+    }
+  }
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    final List<Map<String, Object>> perguntas = [
-      {
-        'texto': 'Qual a sua idade?',
-        'respostas': [15, 20, 25, 27]
-      },
-      {
-        'texto': 'Qual o seu signo?',
-        'respostas': ['Peixes', 'Capricornio', 'Sagitario', 'Touro']
-      },
-      {
-        'texto': 'Qual a sua cor favorita?',
-        'respostas': ['Azul', 'Vermelho', 'Verde', 'Amarelo']
-      },
-    ];
 
-    List<Widget> respostas = [];
-    for(String texto in perguntas[_perguntaSelecionada].cast()['respostas']) {
-      // respostas.add(Resposta(texto,_responder));
-      print(texto);
-    }
+    List<String> respostas = temPerguntaSelecionada ? _perguntas[_perguntaSelecionada].cast()['respostas'] : [];
+
+    // Forma imperativa      
+    // List<Widget> widgets = [];
+    // for(String texto in respostas) {
+    //   widgets.add(Resposta(texto,_responder));
+    // }
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Column(
+        body: temPerguntaSelecionada ? Column(
           children: [
-            Questao(perguntas[_perguntaSelecionada]['texto'].toString()),
-            ...respostas
+            Questao(_perguntas[_perguntaSelecionada]['texto'].toString()),
+            ...respostas.map((t) => Resposta(t, _responder))
           ],
-        )
+        ) : const Center(
+          child: Text(
+            'Parabens',
+            style: TextStyle(fontSize: 28),
+          ),
+        ),
       ),
     );
   }
